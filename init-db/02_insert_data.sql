c796b044db02c2010e82dde1ebd3ce89bf5eb066
Sminor727 <slminor9@gmail.com>
2025-11-09 23:40:58 -0500

    branch for submission

--- init-db/02_insert_data.sql
+++ init-db/02_insert_data.sql
@@ -13,15 +13,6 @@ INSERT INTO events (title, description, start_time, end_time, capacity, status, 
 ('Intro to SQL', 'Hands-on session for SQL basics.', NOW(), NOW() + INTERVAL '2 HOURS', 40, 'published', 1, 2),
 ('Career Night with Alumni', 'Panel and networking with alumni.', NOW() + INTERVAL '1 DAY', NOW() + INTERVAL '1 DAY 3 HOURS', 200, 'published', 1, 1);
 
-SELECT register_user(1,2);
-SELECT register_user(1,3);
-SELECT register_user(1,4);
-SELECT register_user(1,5);
-SELECT register_user(2,1);
-
-UPDATE attendees SET status='canceled'
-WHERE event_id=1 AND user_id=3;
-
 INSERT INTO feedback (user_id, event_id, rating, comments, suggestions) VALUES
 (1, 1, 5, 'Great event!', 'More hands-on activities'),
 (2, 2, 4, 'Informative', 'Longer Q&A session');
